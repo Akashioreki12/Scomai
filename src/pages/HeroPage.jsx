27f41e0e7c54d5078c68701fe9f47b2d3aeea431
Yassine-Kial <kialmohamedyassine@gmail.com>
2023-10-03 08:51:42 +0100

    frijg

---
+++ src/pages/HeroPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+function HeroPage()
+{
+    
+    return (
+        <div>
+            <p>teeeeeessssst</p>
+            <p className="text-red">Custom text</p>
+        </div>
+    );
+}
+
+export default HeroPage;
